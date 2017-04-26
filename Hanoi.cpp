289601b148e31f48680e7ec7f9d352da5906d04b
Jere Benitez <jerebntz@gmail.com>
2017-04-26 09:05:50 +0000

    added Hanoi header and source files. TODO: implement three solve_* methods.

---
+++ Hanoi.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+#include "Hanoi.h"
+
+using std::cout;        using std::stack;
+using std::endl;
+
+Hanoi::Hanoi(int NoD){
+    populate(A);
+    clear(B);
+    clear(C);
+}
+
+void Hanoi::populate(stack<int> tower){
+    for (int i = n; i > 0; i--){
+        tower.push(i);
+    }
+}
+
+void Hanoi::clear(stack<int> tower){
+    while(!tower.empty()){
+        tower.pop();
+    }
+}
+
+void Hanoi::reset(){
+    clear(A);
+    clear(B);
+    clear(C);
+
+    populate(A);
+}
+
+void Hanoi::solve_iterative(){
+
+}
+
+void Hanoi::solve_recursion_simple(){
+
+}
+
+void Hanoi::solve_recursion_double(){
+
+}
---
+++ Hanoi.h
@@ -0,0 +1,50 @@
+#include <stack>
+
+class Hanoi {
+    private:
+        /** Tres colas representando cada torre.
+          */
+        std::stack<int> A, B, C;
+
+        /** Cantidad de discos.
+          */
+        int n;
+
+        /** Mueve el disco superior entre una torre y otra.
+            @param origin Torre de donde sacar el disco.
+            @param objective Torre en donde colocarlo.
+          */
+        void move();
+
+        /** Llena la torre 'tower' con "discos" en orde creciente.
+            @param tower Torre a llenar.
+          */
+        void populate(std::stack<int> tower);
+        
+        /** Saca todos los discos de la torre 'tower'.
+            @param tower Torre a vaciar.
+          */
+        void clear(std::stack<int> tower);
+
+    public:
+        /** Constructor.
+            @param NoD Número de discos.
+          */
+        Hanoi(int NoD);
+
+        /** Resuelve el puzzle con un método iterativo.
+          */
+        void solve_iterative();
+        
+        /** Resuelve el puzzle con un método recursivo simple.
+          */ 
+        void solve_recursion_simple();
+        
+        /** Resuelve el puzzle con un étodo recursivo doble.
+          */ 
+        void solve_recursion_double();
+
+        /** Resetea el sistema al estado original.
+          */
+        void reset();
+};
