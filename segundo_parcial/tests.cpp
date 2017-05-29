2e83dae8c8f3e2ac3ef5808a5d7648322465ef0c
Jere Benitez <jerebntz@gmail.com>
2017-05-29 22:13:08 +0000

    test basico para AVL y heap)

--- segundo_parcial/tests.cpp
+++ segundo_parcial/tests.cpp
@@ -28,9 +28,29 @@ int main() {
 	lista.from_tree(arbol.get_root());
 	cout << lista.to_string();
 
-	cout << "Ordenando lista" << endl << endl;
-	lista.sort_insertion();
+	cout << "Ordenando lista (heap)" << endl << endl;
+	lista.sort_heap();
 	cout << lista.to_string();
+
+	cout << "Creando arbol AVL" << endl;
+	structures::TreeAVL avl;
+
+	for (it = list_of_words.begin(); it != list_of_words.end(); it++) {
+		cout << "Agregando: " << *it << endl;
+		if (avl.has(*it)) avl.increment_reps(*it);
+		else avl.node_add(*it);
+		cout << *it << " agregado." << "Repeticiones: " 
+		     << avl.find(*it)->repetitions << endl;
+	}
+
+	cout << "Creando lista" << endl;
+	structures::List lista2;
+	lista2.from_tree(avl.get_root());
+	cout << lista2.to_string();
+
+	cout << "Ordenando lista" << endl << endl;
+	lista2.sort_insertion();
+	cout << lista2.to_string();	
 
 	return 0;
 }
