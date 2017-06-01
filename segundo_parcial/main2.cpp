d206dac77128bc354578e8a675dcb6d27e95b700
GabrielEValenzuela <gabrielvalenzuela12@gmail.com>
2017-05-31 22:34:33 -0300

    Update main2.cpp
    V2.0

--- segundo_parcial/main2.cpp
+++ segundo_parcial/main2.cpp
@@ -1,7 +1,9 @@
+#include "stdafx.h" //NO BORRAR
 #include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 #include "structures.h"
 
@@ -17,6 +19,10 @@ void iterative_method(ifstream&, structures::TreeAVL& tree, string tree_type);
 void recursive_method(ifstream&, structures::TreeAVL& tree, string tree_type);
 fstream _open_file(string);
 void _write_file(string, string);
+void no_puntuations_marks(string &);
+//
+const string MARKS = ".;:;()[]{}<>";
+//
 
 int main() {
 	string file_name, tree_type;
@@ -24,44 +30,55 @@ int main() {
 	structures::TreeAVL tree;
 	char opt;
 
+	string s1 = ".Prueba.";
+	string s2 = "<>.Prueba;2.<>";
+
+	cout << s1 << "  " << "  " << s2 << endl;
+	cout << "Llamo a metodo no..." << endl;
+	no_puntuations_marks(s1);
+	no_puntuations_marks(s2);
+	cout << s1 << "  " << "  " << s2 << endl;
+
+
 	main_menu(opt);
 
 	switch (opt) {
-		case 'o':
-			cout << "Ingrese el nombre del archivo: ";
-			cin >> file_name;
-			try {
-				f.open(file_name.c_str());
-			} catch (std::exception &ex) {
-				cout << ex.what();
-			}
-			break;
-		case 'x':
-			return 0;
+	case 'o':
+		cout << "Ingrese el nombre del archivo: ";
+		cin >> file_name;
+		try {
+			f.open(file_name.c_str());
+		}
+		catch (std::exception &ex) {
+			cout << ex.what();
+		}
+		break;
+	case 'x':
+		return 0;
 	}
 
 	structure_menu(opt);
 	switch (opt) {
-		case 'a':
-			tree_type = "AVL";
-			break;
-		case 'b':
-			tree_type = "BTS";
-			break;
-		case 'x':
-			return 0;
+	case 'a':
+		tree_type = "AVL";
+		break;
+	case 'b':
+		tree_type = "BTS";
+		break;
+	case 'x':
+		return 0;
 	}
 
 	method_menu(opt);
 	switch (opt) {
-		case 'i':
-			iterative_method(f, tree ,tree_type);
-			break;
-		case 'r':
-			recursive_method(f, tree ,tree_type);
-			break;
-		case 'x':
-			return 0;
+	case 'i':
+		iterative_method(f, tree, tree_type);
+		break;
+	case 'r':
+		recursive_method(f, tree, tree_type);
+		break;
+	case 'x':
+		return 0;
 	}
 
 	structures::List lista;
@@ -69,91 +86,94 @@ int main() {
 
 	sorting_menu(opt);
 	switch (opt) {
-		case 'i':
-			cout << "Ordenando utilizando método de insercion." << endl;
-			lista.sort_insertion();
-			break;
-		case 'h':
-			cout << "Ordenando utilizando método heap." << endl;
-			lista.sort_heap();
-			break;
-		case 'x':
-			return 0;
+	case 'i':
+		cout << "Ordenando utilizando método de insercion." << endl;
+		lista.sort_insertion();
+		break;
+	case 'h':
+		cout << "Ordenando utilizando método heap." << endl;
+		lista.sort_heap();
+		break;
+	case 'x':
+		return 0;
 	}
 
 	string result = lista.to_string();
 	string out_file;
 
 	end_menu(opt);
+
 	switch (opt) {
-		case 'b':
-			cout << result << endl;
-		case 'p':
-			cout << "Imprimiendo a archivo. Ingrese el nombre: ";
-			cin >> out_file;
-			_write_file(out_file, result);
-			break;
-		case 's':
-			cout << result << endl;
-			break;
-		case 'x':
-			return 0;
+	case 'b':
+		cout << result << endl;
+	case 's':
+		cout << "Imprimiendo a archivo. Ingrese el nombre: ";
+		cin >> out_file;
+		_write_file(out_file, result);
+		break;
+	case 'd':
+		cout << result << endl;
+		break;
+	case 'x':
+		return 0;
 	}
+
+	system("PAUSE");
 
 	return 0;
 }
 
 void main_menu(char& opt) {
 	cout << " ----- Menu Principal -----" << endl
-		 << "Elija una opcion: " << endl
-		 << "'o' -- Abrir archivo" << endl
-		 << "'x' -- Salir" << endl
-		 << ">> ";
+		<< "Elija una opcion: " << endl
+		<< "'o' -- Abrir archivo" << endl
+		<< "'x' -- Salir" << endl
+		<< ">> ";
 
 	cin >> opt;
 }
 
 void structure_menu(char& opt) {
 	cout << " ----- Menu Estructura -----" << endl
-		 << "Elija una estructura: " << endl
-		 << "'a' -- AVL" << endl
-		 << "'b' -- BTS" << endl
-		 << "'x' -- Salir" << endl
-		 << ">> ";
+		<< "Elija una estructura: " << endl
+		<< "'a' -- AVL" << endl
+		<< "'b' -- BTS" << endl
+		<< "'x' -- Salir" << endl
+		<< ">> ";
 
 	cin >> opt;
 }
 
 void sorting_menu(char& opt) {
 	cout << " ----- Menu Ordenamiento -----" << endl
-		 << "Elija un metodo de ordenamiento: " << endl
-		 << "'h' -- Heap" << endl
-		 << "'i' -- Insertion" << endl
-		 << "'x' -- Salir" << endl
-		 << ">> ";
+		<< "Elija un metodo de ordenamiento: " << endl
+		<< "'h' -- Heap" << endl
+		<< "'i' -- Insertion" << endl
+		<< "'x' -- Salir" << endl
+		<< ">> ";
 
 	cin >> opt;
 }
 
 void method_menu(char& opt) {
 	cout << " ----- Menu Metodo Llenado -----" << endl
-		 << "Elija un metodo llenado: " << endl
-		 << "'i' -- Iterativo" << endl
-		 << "'r' -- Recursivo" << endl
-		 << "'x' -- Salir" << endl
-		 << ">> ";
+		<< "Elija un metodo llenado: " << endl
+		<< "'i' -- Iterativo" << endl
+		<< "'r' -- Recursivo" << endl
+		<< "'x' -- Salir" << endl
+		<< ">> ";
 
 	cin >> opt;
 }
 
 void end_menu(char& opt) {
 	cout << " ----- Menu Finalizacion -----" << endl
-		 << "Elija que hacer con el resultado: " << endl
-		 << "'s' -- Mostrar" << endl
-		 << "'p' -- Imprimir a archivo" << endl
-		 << "'b' -- Ambas" << endl
-		 << "'x' -- Salir" << endl
-		 << ">> ";
+		<< "Elija que hacer con el resultado: " << endl
+		<< "'d' -- Mostrar" << endl
+		<< "'s' -- Imprimir a archivo" << endl
+		<< "'b' -- Ambas" << endl
+		<< "'x' -- Salir" << endl
+		<< ">> ";
 
 	cin >> opt;
 }
@@ -181,7 +201,7 @@ void iterative_method(ifstream& f, structures::TreeAVL& tree, string tree_type) 
 
 void recursive_method(ifstream& f, structures::TreeAVL& tree, string tree_type) {
 	string buffer;
-	if (cin >> buffer) {
+	if (f >> buffer) {
 		std::transform(buffer.begin(), buffer.end(), buffer.begin(), ::tolower);
 		if (tree.has(buffer)) tree.increment_reps(buffer);
 		else {
@@ -191,3 +211,25 @@ void recursive_method(ifstream& f, structures::TreeAVL& tree, string tree_type) 
 		recursive_method(f, tree, tree_type);
 	}
 }
+
+bool is_puntuations(char ch) {
+	return MARKS.find(ch) != string::npos;
+}
+
+void no_puntuations_marks(string& str) {
+
+	std::ostringstream buffer;
+
+	string::const_iterator iter = str.begin();
+	char next_ch = *iter;
+		while (iter != str.end())
+		{
+			if (is_puntuations(next_ch)==false)
+			{
+				buffer.put(next_ch);
+				iter++;
+			}
+		}
+
+		str = buffer.str();
+	}
